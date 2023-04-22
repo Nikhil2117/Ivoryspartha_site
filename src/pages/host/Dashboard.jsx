c3915b63025adf57227039253c326234af2f1f83
Nikhil_2117 <chnikhil62@gmail.com>
2023-04-22 23:19:23 +0530

    fixing minor bugs

--- src/pages/host/Dashboard.jsx
+++ src/pages/host/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import makeServer from "../../server/server";
-
+makeServer()
 
 export default function HostVans() {
     const [vans, setVans] = React.useState([])
